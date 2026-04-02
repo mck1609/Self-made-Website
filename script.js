5521f2a606b94d47d374ba07fde5f1e567cf0d0f
Maximilian Cassis Krizmanic <Maximilian-Cassis@web.de>
2026-04-02 21:41:25 +0200

    fix Hero

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const heroImage = document.getElementById("hero-image");
 
-window.addEventListener("scroll", () => {
-  if (!heroImage) return; // Sicherheitshalber, falls Bild fehlt
+// window.addEventListener("scroll", () => {
+//   if (!heroImage) return;
 
-  const scrollY = window.scrollY;
-  const progress = Math.min(scrollY / 900, 1);
+//   const scrollY = window.scrollY;
+//   const progress = Math.min(scrollY / 900, 1);
 
-  const size = 100 - progress * 55; 
-  const moveX = progress * 43;
-  const moveY = progress * -34;
-  const radius = progress * 22;
+//   const size = 100 - progress * 55; 
+//   const moveX = progress * 43;
+//   const moveY = progress * -34;
+//   const radius = progress * 22;
 
-  heroImage.style.width = `${size}vw`;
-  heroImage.style.height = `${size}vw`;
-  heroImage.style.borderRadius = `${radius}px`;
-  
-  // HIER WAR DER FEHLER: Das "=" hat gefehlt
-  heroImage.style.transform = `translate(-50%, -50%) translate(${moveX}vw, ${moveY}vh)`;
-});
+//   heroImage.style.width = `${size}vw`;
+//   heroImage.style.height = `${size}vw`;
+//   heroImage.style.borderRadius = `${radius}px`;
+
+//   heroImage.style.transform = `translate(-50%, -50%) translate(${moveX}vw, ${moveY}vh)`;
+// });
 
 function sendEmail(type) {
   const email = "Maximilian-cassis@web.de";
